9a06a6ff70b7e7fc939170898cf5d066fbed90fc
Léonard Jacob <81411868+JACOB-Leonard@users.noreply.github.com>
2021-06-02 17:15:59 +0200

    Add files via upload

--- Plateau.java
+++ Plateau.java
@@ -1,17 +1,16 @@
-import java.util.*;
 
 public class Plateau {
 	//cases contient donc chaque case du plateau sous forme de tableau de cases.
 	private Case[][] cases;
 	//Le constructeur de plateau.
 	public Plateau(){
-		//On initialise cases avec un tableau de 8 rangÃ©es de 8 cases.
+		//On initialise cases avec un tableau de 8 rangées de 8 cases.
 		this.cases = new Case[8][8];
-		//On parcourt le tableau par les rangÃ©es puis par les cases.
+		//On parcourt le tableau par les rangées puis par les cases.
 		for(int i = 0;i<8;i++){
 			char a = 'A';
 			for(int y = 0;y<8;y++){
-				//si i = 0 alors on crÃ©e les piÃ¨ces 'spÃ©ciales' noirs.
+				//si i = 0 alors on crée les pièces 'spéciales' noirs.
 				if(i == 0){
 					cases[i][0] = new Case(new Tour('A',1),'A',1);
 					cases[i][1] = new Case(new Cavalier('B',1),'B',1);
@@ -23,12 +22,12 @@ public class Plateau {
 					cases[i][7] = new Case(new Tour('H',1),'H',1);
 					y = 8;
 				}
-				//si i = 1/6 alors on crÃ©e une rangÃ©e de pion noirs/blancs.
+				//si i = 1/6 alors on crée une rangée de pion noirs/blancs.
 				else if(i == 1 || i == 6){
 					this.cases[i][y] = new Case(new Pion(a,i+1),a,i+1);
 					a++;
 				}
-				//si i = 7 alors on crÃ©e les piÃ¨ces 'spÃ©ciales' blanches.
+				//si i = 7 alors on crée les pièces 'spéciales' blanches.
 				else if(i == 7){
 					cases[i][0] = new Case(new Tour('A',8),'A',8);
 					cases[i][1] = new Case(new Cavalier('B',8),'B',8);
@@ -40,7 +39,7 @@ public class Plateau {
 					cases[i][7] = new Case(new Tour('H',8),'H',8);
 					y = 8;
 				}
-				//Pour toutes les autres situations, nous crÃ©eons des cases vides.
+				//Pour toutes les autres situations, nous créeons des cases vides.
 				else{	
 					this.cases[i][y] = new Case(a,i+1);
 					a++;
@@ -48,7 +47,7 @@ public class Plateau {
 			}
 		}
 	}
-	//Comme nous utilisons des charactÃ¨res et qu'il est impossible de parcourir des tableaux Ã  l'aide de charactÃ¨re, cette fonction permet de translater le caractÃ¨re en chiffre.
+	//Comme nous utilisons des charactères et qu'il est impossible de parcourir des tableaux à l'aide de charactère, cette fonction permet de translater le caractère en chiffre.
 	public int chartoInt(char c){
 		int a;
 		if(c == 'A'){
@@ -80,7 +79,7 @@ public class Plateau {
 		}
 		return a;
 	}
-	//Cette mÃ©thode permet de vÃ©rifier qu'il y a un echec. C'est Ã  dire que le roi peut Ãªtre manger par une piÃ¨ce de la couleur adverse.
+	//Cette méthode permet de vérifier qu'il y a un echec. C'est à dire que le roi peut être manger par une pièce de la couleur adverse.
 	public String echec(){
 		//Ces variables permettent de retenir l'emplacement des rois.
 		int rb = 0;
@@ -104,18 +103,20 @@ public class Plateau {
 				}
 			}
 		}
-		//AprÃ¨s avoir obtenu les coordonnÃ©es des deux rois, le tableau va Ãªtre parcouru de nouveau et chacune des piÃ¨ces du plateau vont Ãªtre testÃ©s afin de savoir si elles ont la posibilitÃ© de se dÃ©placer sur la case du roi de la couleur adverse. Si oui, Un Ã©chec a lieu. Si non, Rien ne se passe.
+		//Après avoir obtenu les coordonnées des deux rois, le tableau va être parcouru de nouveau et chacune des pièces du plateau vont être testés afin de savoir si elles ont la posibilité de se déplacer sur la case du roi de la couleur adverse.
+		//Si oui, Un échec a lieu. Si non, Rien ne se passe.
 		for(int i = 0; i<8;i++){
 			for(int y = 0; y<8;y++){
 				if(this.cases[i][y].contenu != null){
-					//Si la piÃ¨ce est noir, .deplacement va Ãªtre testÃ© pour les coordonnÃ©es du roi blanc. Si elle peut y aller alors il y a un echec blanc. 
+					
+					//Si la pièce est noir, .deplacement va être testé pour les coordonnées du roi blanc. Si elle peut y aller alors il y a un echec blanc. 
 					if(this.cases[i][y].contenu.couleur == "Noir"){
 						if(this.cases[i][y].contenu.deplacement(cb,rb) == true){
 							return "Echec Blanc";
 						}
 					}
 					
-					//L'inverse si la piÃ¨ce est blanche.
+					//L'inverse si la pièce est blanche.
 					else if(this.cases[i][y].contenu.couleur == "Blanc"){
 						if(this.cases[i][y].contenu.deplacement(cn,rn) == true){
 							return "Echec Noir";
@@ -127,31 +128,39 @@ public class Plateau {
 		return "Pas Echec";
 	}
 				
-	//Cette mÃ©thode permet de tester si une piÃ¨ce peut Ãªtre dÃ©placÃ© d'une case Ã  une autre dans la configuration de plateau actuelle.
+	//Cette méthode permet de tester si une pièce peut être déplacé d'une case à une autre dans la configuration de plateau actuelle.
 	public void deplacementPiece(char c1,int r1, char c2, int r2){
-		//Utilise chartoInt pour transformer les charactÃ¨res en chiffre(coordonnÃ©es)
-		int c11 = this.chartoInt(c1);
-		int c12 = this.chartoInt(c2);
-		//Teste si les deux cases sÃ©lectionnÃ©s sont les mÃªmes ou si la premiÃ¨re case choisi est vide.
-		if(this.cases[r1-1][c11] != this.cases[r2-1][c12] && this.cases[r1-1][c11].contenu != null){
+		
+		//Utilise chartoInt pour transformer les charactères en chiffre(coordonnées)
+		int x1 = this.chartoInt(c1);
+		int x2 = this.chartoInt(c2);
+		Case depart = this.cases[r1-1][x1];
+		Case arrive = this.cases[r2-1][x2];
+		
+		//Teste si les deux cases sélectionnés sont les mêmes ou si la première case choisi est vide.
+		if(depart != this.cases[r2-1][x2] && depart.contenu != null){
+			
 			//Teste si la case est vide:
-			if(this.cases[r2-1][c12].contenu != null){
-				//Si elle ne l'est pas, alors il ya une vÃ©rification pour voir si les deux cases sont de la mÃªme couleur, si elles sont pas alors il y a un test de la mÃ©thode deplacement de la piÃ¨ce.
-				if(this.cases[r1-1][c11].contenu.memeCouleur(this.cases[r2-1][c12].contenu) != true){
-					if(this.cases[r1-1][c11].getContenu().deplacement(c2,r2) == true){
-						this.cases[r2-1][c12].contenu = this.cases[r1-1][c11].contenu;
-						this.cases[r1-1][c11].contenu = null;
+			if(arrive.contenu != null){
+				
+				//Si elle ne l'est pas, alors il ya une vérification pour voir si les deux cases sont de la même couleur, si elles sont pas alors il y a un test de la méthode deplacement de la pièce.
+				if(depart.contenu.memeCouleur(arrive.contenu) != true){
+					if(depart.getContenu().deplacement(c2,r2) == true){
+						arrive.contenu = depart.contenu;
+						depart.contenu = null;
 					}
 				}
-			}	
-			//so elle l'est alors le dÃ©placement a lieu aprÃ¨s vÃ©rification de la mÃ©thode deplacement de la piÃ¨ce.
-			else if(this.cases[r1-1][c11].getContenu().deplacement(c2,r2) == true){
-				this.cases[r2-1][c12].contenu = this.cases[r1-1][c11].contenu;
-				this.cases[r1-1][c11].contenu = null;
+			}
+			
+			//so elle l'est alors le déplacement a lieu après vérification de la méthode deplacement de la pièce.
+			else if(depart.getContenu().deplacement(c2,r2) == true){
+				arrive.contenu = depart.contenu;
+				depart.contenu = null;
 			}
 		}
 		this.affichage();
 	}
+	
 	//Permet d'afficher le plateau en 'scannant' le contenu de chacune des cases et d'afficher leur 'Apparence'
 	public void affichage(){
 		String s;
@@ -171,5 +180,13 @@ public class Plateau {
 			System.out.println(s);
 		}
 		System.out.println(" "+"----------------------------------");
-	}		
+	}
+
+	public void jouerPartie(String [] tPartie){
+		for(int n=0; n<tPartie.length; n++) {
+			String lecture = tPartie[n];
+			deplacementPiece(lecture.charAt(0), lecture.charAt(1)-'0',  lecture.charAt(2), lecture.charAt(3)-'0');
+		}
+	}
+	
 }
--- PlateauTest.java
+++ PlateauTest.java
@@ -1,38 +1,25 @@
-import java.util.*;
 
 public class PlateauTest {
 	
 	public static void main(String[] args) {
 		Plateau plateaudejeu = new Plateau();
-		plateaudejeu.affichage();
-		plateaudejeu.deplacementPiece('B',7,'B',6);
-		plateaudejeu.deplacementPiece('B',6,'B',5);
-		plateaudejeu.deplacementPiece('B',5,'B',6);
-		plateaudejeu.deplacementPiece('B',2,'B',3);
-		plateaudejeu.deplacementPiece('B',3,'B',4);
-		plateaudejeu.deplacementPiece('B',4,'B',5);
-		plateaudejeu.deplacementPiece('C',7,'C',5);
-		plateaudejeu.deplacementPiece('C',5,'C',3);
-		plateaudejeu.deplacementPiece('B',8,'C',6);
-		plateaudejeu.deplacementPiece('C',6,'E',5);
-		plateaudejeu.deplacementPiece('E',5,'D',3);
-		plateaudejeu.deplacementPiece('D',3,'C',5);
-		plateaudejeu.deplacementPiece('C',8,'A',6);
-		plateaudejeu.deplacementPiece('A',6,'C',4);
-		plateaudejeu.deplacementPiece('C',4,'E',6);
-		plateaudejeu.deplacementPiece('A',8,'A',1);
-		plateaudejeu.deplacementPiece('H',8,'A',8);
-		plateaudejeu.deplacementPiece('A',8,'B',5);
-		plateaudejeu.deplacementPiece('D',8,'H',8);
-		plateaudejeu.deplacementPiece('H',8,'H',5);
-		plateaudejeu.deplacementPiece('H',5,'E',2);
-		plateaudejeu.deplacementPiece('E',2,'F',4);
-		plateaudejeu.deplacementPiece('E',8,'D',8);
-		plateaudejeu.deplacementPiece('D',8,'C',7);
-		plateaudejeu.deplacementPiece('C',7,'C',6);
-		plateaudejeu.deplacementPiece('C',6,'A',6);
+		//plateaudejeu.affichage();
+		//plateaudejeu.deplacementPiece('C',7,'C',6);
+		//plateaudejeu.deplacementPiece('B',7,'B',6);
+		//plateaudejeu.deplacementPiece('C',8,'A',6);
+		//plateaudejeu.deplacementPiece('D',8,'C',8);
+		//plateaudejeu.deplacementPiece('B',1,'A',3);
+
+		//Future fichier texte
+		final String[] partie = {
+				"D2D4",
+				"D7D5",
+				"C2C4",
+				"C7C6",
+				"B1C3",
+				"G8F6",
+				"H2H3"};
 		
+		plateaudejeu.jouerPartie(partie);
 	}
 }
-			
-			
